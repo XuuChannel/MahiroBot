0d3c3096e6cbc7b2aa94ddac868edfc2846127b8
XuuChannel <67725421+XuuChannel@users.noreply.github.com>
2023-04-21 15:04:05 +0900

    Update classic.py

--- module/classic.py
+++ module/classic.py
@@ -1,9 +1,10 @@
-#先测试下线程锁
 from core import message
 from core import bot
 import json
 import logging
 import _thread
+import time
+import os
 
 mahiroModuleInfo = {
     "name":"MahiroClassic",
@@ -15,22 +16,27 @@ mahiroModuleInfo = {
     "target":"target"
 }
 
-initFlag = False
 classicList = []
 cLock = _thread.allocate_lock()
 
+try:
+    f = open("./data/classic/classic.json","r", encoding="utf-8")
+    classicList = json.load(f)["list"]
+    f.close()
+except FileNotFoundError:
+    f = open("./data/classic/classic.json","w", encoding="utf-8")
+    s = {"list":[]}
+    json.dump(s, f, ensure_ascii=False)
+    f.close()
+logging.info("Module MahiroClassic INIT succeed.")
+
 def mahiroModule(bot:bot.Bot,inbound:message.Chain=None,evinbound:message.Event=None)->None:
     global classicList
-    global initFlag
-    if(initFlag == False):
-        try:
-            f = open("./data/classic/classic.json","r", encoding="utf-8")
-            classicList = json.load(f)["list"]
-            f.close()
-        except FileNotFoundError:
-            f = open("./data/classic/classic.json","w", encoding="utf-8")
-            s = {"list":[]}
-            json.dump(s, f, ensure_ascii=False)
-            f.close()
-        logging.info("Module MahiroClassic INIT succeed.")
-        initFlag = True
+    global cLock
+    while(cLock.locked()):
+        time.sleep(0.1)
+    cLock.acquire()
+    for i in range(20):
+        time.sleep(1)
+        print("lockTest:"+str(i))
+    cLock.release()
