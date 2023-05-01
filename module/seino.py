d98f41534008e0ee24b8c312b3968b1e3ef86408
Xuu <67725421+XuuChannel@users.noreply.github.com>
2023-05-02 03:54:20 +0900

    Update seino.py

--- module/seino.py
+++ module/seino.py
@@ -2,12 +2,12 @@
 #   安装依赖: pip install psutil
 from core import message
 from core import bot
-import psutil
+import psutil,os
 import _thread,time
-
+#代码文件名中带有字符"p"好像无法正常识别
 mahiroModuleInfo = {
     "name":"Performance",
-    "version":1.0,
+    "version":2.0,
     "type":"trigger",
     "condition":"Command",
     "command":["P"],
@@ -17,12 +17,24 @@ mahiroModuleInfo = {
 
 cLock = _thread.allocate_lock()
 
+def cpuInfo():
+    cpuTimes = psutil.cpu_times()
+    # 获取CPU信息中的内存信息
+    def memoryInfo(memory):
+        return '\n    总内存(total): '+ str(round((float(memory.total) / 1024 / 1024 / 1024), 2)) + "G"+'\n    已使用(used): '+ str(round((float(memory.used) / 1024 / 1024 / 1024), 2)) + "G"+'\n    空闲(free): '+ str(round((float(memory.free) / 1024 / 1024 / 1024), 2)) + "G"+'\n    使用率(percent): '+ str(memory.percent) + '%'
+    return '物理CPU(core): '+str(psutil.cpu_count(logical=False))+'\n逻辑CPU个数(thread): '+str(psutil.cpu_count())+'\n虚拟内存(virtual_memory)'+memoryInfo(psutil.virtual_memory())+'\n交换内存(swap_memory)'+memoryInfo(psutil.swap_memory())
+
 def mahiroModule(bot:bot.Bot,inbound:message.Chain=None,evinbound:message.Event=None)->None:
     global cLock
     while(cLock.locked()):
         time.sleep(0.1)
     cLock.acquire()
-    perfmessage = "以下是bot宿主机的状态信息喵\n\nCPU("+str(psutil.cpu_count(logical=False))+"C"+str(psutil.cpu_count())+"T):\n"+str(psutil.cpu_times())+"\ncpu_percent"+str(psutil.cpu_percent(interval=3,percpu=True))+"\n\nMemory:\n"+str(psutil.virtual_memory())
+    pinfo = psutil.Process(os.getpid())
+    perfmessage = "以下为机盖宁的进程信息喵\n\n内存占用: "+str(round(pinfo.memory_info().rss/1024/1024,2))+"MB ("+str(round(pinfo.memory_percent(),1))+"%)\nCPU占用: "+str(pinfo.cpu_percent(interval=3))+"%"
+    inbound.chainClear()
+    inbound.add(message.Plain(perfmessage))
+    inbound.send(bot)
+    perfmessage = "以下为宿主机的信息喵\n\n"+cpuInfo()
     inbound.chainClear()
     inbound.add(message.Plain(perfmessage))
     inbound.send(bot)
